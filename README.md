3ccf99014aba89df3d994cf3a843e93b597c0445
Steven Willmott <stevexw@gmail.com>
2012-07-21 20:15:50 +0200

    Version 2 including proper sentiment and Analytics

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ This is the top level README file which provides an index to the versions availa
 
  * Version 1: First video example using GRAPE, 3scale and Heroku <a href="version_1/README.md">[README]</a>.
 
+ * Version 1: Second video example adding proper sentiment analysis and API Analytics <a href="version_2/README.md">[README]</a>.
+
 The API is meant to serve an example of how you can easily create an API, and how you can use the free 3scale platform to control, manage and monitor the operations of the API so that you only have to do the fun part.
 
 The original sentiment values are taken from the dataset AFINN-111.txt from [Technical University of Denmark](http://www2.imm.dtu.dk/pubdb/views/publication_details.php?id=6010).
---
+++ version_2/app/sentimentapi_v2.1.rb
@@ -0,0 +1,53 @@
+require 'rubygems'
+require 'grape'
+require 'json'
+require '3scale/client'
+require "#{File.dirname(__FILE__)}/analyzer"
+
+class SentimentApiV2_1 < Grape::API
+  version 'v2.1', :using => :path, :vendor => '3scale'
+  error_format :json
+  
+  #Links to 3scale infrastructure
+  $client = ThreeScale::Client.new(:provider_key => "db0e2b4bb3f2ceb3fbb00302a39f2cf8")
+  
+  #Sentiment Logic component
+  @@the_logic = Analyzer.new
+  
+  helpers do
+    def authenticate!
+      response = $client.authorize(:app_id => params[:app_id], :app_key => params[:app_key])
+      error!('403 Unauthorized', 403) unless response.success?
+    end
+    
+    def report!(method_name='hits', usage_value=1)      
+      response = $client.report({:app_id => params[:app_id],  :usage => {method_name => usage_value}})
+      error!('505 Reporting Error', 505) unless response.success?
+    end
+    
+  end
+  
+  resource :words do
+    get ':word' do
+        authenticate!
+        report!('word/get', 1)
+        @@the_logic.word(params[:word]).to_json
+    end
+    
+    post ':word' do
+      authenticate!
+      report!('word/post', 1)
+      res =  @@the_logic.add_word(params[:word],params[:value])
+      res.to_json
+    end 
+  end
+  
+  resource :sentences do
+    get ':sentence' do
+      authenticate!
+      report!('sentence/get', 1)
+      @@the_logic.sentence(params[:sentence]).to_json
+    end
+  end
+
+end
